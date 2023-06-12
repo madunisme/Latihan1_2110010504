c98be72a321d9bafa4fddba7a058f8b5fb9823a9
madunisme <machabubble19@gmail.com>
2023-06-12 19:25:32 +0800

    Membuat Class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -3,5 +3,13 @@ package hitung;
 
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        // Membuat Objek
+        Matematika madun = new Matematika(0,4);
+        
+        System.out.println("Hasil Penjumlahan : " + madun.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : " + madun.setPengurangan());
+        System.out.println("Hasil Perkalian   : " + madun.setPerkalian());
+        System.out.println("Hasil Pembagian   : " + madun.setPembagian());
+    }
 }
