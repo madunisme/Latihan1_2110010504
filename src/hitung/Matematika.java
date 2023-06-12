0714123a5abb5fa9a21fc9381ca159be72d391c8
madunisme <machabubble19@gmail.com>
2023-06-12 19:09:37 +0800

    Membuat Constructor dan setPenjumlahan

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -3,5 +3,18 @@ package hitung;
 
 
 public class Matematika {
+    // Membuat Variable
+    private double bil1, bil2;
+    
+    // Membuat Constructor
+    public Matematika(double bil1, double bil2){
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+    }
+    
+    // Membuat Method setPenjumlahan
+    public double setPenjumlahan(){
+        return bil1 + bil2;
+    }
     
 }
